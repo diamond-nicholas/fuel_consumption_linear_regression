f7ad769b8d8d39ebb9bbca4b40662e4dc40f0619
Diamond Nicholas <diamondnicholas@apples-MacBook-Pro.local>
2022-05-22 13:15:21 +0100

    get accuracy and mae

--- main.py
+++ main.py
@@ -70,3 +70,12 @@ plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
 plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
+
+# get accuracy and mae(mean absolute error)
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_new = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_new - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_new - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y , test_y_new) )
