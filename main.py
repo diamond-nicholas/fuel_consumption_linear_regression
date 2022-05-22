1b2403e8758678d03237840fbfb19972ba870cc5
Diamond Nicholas <diamondnicholas@apples-MacBook-Pro.local>
2022-05-22 12:58:51 +0100

    read data

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 # %matplotlib inline
+
+!wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
+
+# read data
+data = pd.read_csv("FuelConsumptionCo2.csv")
+data.head()
